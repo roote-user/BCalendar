4dfb7aad13dab339e83c4f2182f9fc3cde12a2e3
roote-user <ryan.j.coote@gmail.com>
2022-07-06 13:40:20 -0400

    Initial Package Commit

---
+++ src/com/Bcalendar/Main.java
@@ -0,0 +1,13 @@
+package com.Bcalendar;
+
+/*
+    Began by Ryan C.
+    Started 7/6/22
+ */
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("BCalendar Start Up Registered");
+        System.out.println("Exiting BCalendar");
+    }
+}
